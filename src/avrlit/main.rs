a4f2910cebc1aaaea9ec685f2acc02ef4f0db82b
Dylan McKay <me@dylanmckay.io>
2017-07-06 10:06:54 +1200

    Split avrlit into its own executable

---
+++ src/avrlit/main.rs
@@ -0,0 +1,23 @@
+extern crate lit;
+
+const CRATE_PATH: &'static str = env!("CARGO_MANIFEST_DIR");
+
+/// Information about a compiler.
+#[derive(Debug)]
+struct Compiler {
+    executable_path: &'static str,
+}
+
+const AVR_GCC: Compiler = Compiler { executable_path: "avr-g++" };
+
+fn main() {
+    let compiler = AVR_GCC;
+
+    lit::run::tests(|config| {
+        config.add_search_path(format!("{}/tests", CRATE_PATH));
+        config.add_extension("cpp");
+
+        config.constants.insert("cxx".to_owned(), compiler.executable_path.to_owned());
+    })
+}
+
