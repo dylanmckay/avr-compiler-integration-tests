4f098e72d3f06ac89a5aa38baa623a2e6902e31d
Dylan McKay <me@dylanmckay.io>
2020-11-19 00:04:31 +1300

    Expand u8 shift tests
    https://reviews.llvm.org/D89047

--- tests/unit/bitwise/shift_u8.cpp
+++ tests/unit/bitwise/shift_u8.cpp
@@ -23,10 +23,22 @@ void unit_test(void) {
   OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 0);
   // CHECK: changed(OUTPUT_VALUE) = 4
   OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 1);
+  // CHECK: changed(OUTPUT_VALUE) = 8
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 2);
+  // CHECK: changed(OUTPUT_VALUE) = 16
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 3);
+  // CHECK: changed(OUTPUT_VALUE) = 32
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 4);
+  // CHECK: changed(OUTPUT_VALUE) = 64
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 5);
   // CHECK: changed(OUTPUT_VALUE) = 128
-  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(1, 7);
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(2, 6);
+  // CHECK: changed(OUTPUT_VALUE) = 0
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(1, 8);
   // CHECK: changed(OUTPUT_VALUE) = 120
   OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(30, 2);
+  // CHECK: changed(OUTPUT_VALUE) = 128
+  OUTPUT_VALUE = shift_left<uint8_t, uint8_t>(1, 7);
 
   // Shift right:
   // CHECK: changed(OUTPUT_VALUE) = 127
@@ -37,5 +49,23 @@ void unit_test(void) {
   OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(50, 10); // shift completely out
   // CHECK: changed(OUTPUT_VALUE) = 50
   OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(200, 2);
+  // CHECK: changed(OUTPUT_VALUE) = 128
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 0);
+  // CHECK: changed(OUTPUT_VALUE) = 64
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 1);
+  // CHECK: changed(OUTPUT_VALUE) = 32
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 2);
+  // CHECK: changed(OUTPUT_VALUE) = 16
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 3);
+  // CHECK: changed(OUTPUT_VALUE) = 8
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 4);
+  // CHECK: changed(OUTPUT_VALUE) = 4
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 5);
+  // CHECK: changed(OUTPUT_VALUE) = 2
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 6);
+  // CHECK: changed(OUTPUT_VALUE) = 1
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 7);
+  // CHECK: changed(OUTPUT_VALUE) = 0
+  OUTPUT_VALUE = shift_right<uint8_t, uint8_t>(128, 8);
 }
 
